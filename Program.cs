14e92039d3d09422f0627b651bf69c80667ab733
Tiago Rodrigues <isatiago098@gmail.com>
2021-11-03 16:02:31 -0300

    resoluçao exercio maiordoisnumeros

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace maiordoisnumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiro, segundo, maiornumeros;
+            Console.Write(" primeiro numero..");
+            primeiro = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("segundo numero....");
+            segundo = Convert.ToInt32(Console.ReadLine());
+
+            if (primeiro>segundo)
+            {
+                maiornumeros = primeiro;
+            }
+            else
+            {
+                maiornumeros = segundo;
+            }
+
+            Console.WriteLine($"maior numero é {maiornumeros}");
+
+
+
+        }
+    }
+}
